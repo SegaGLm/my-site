5c0c64d37be2ba1331af2b0f6a9e8acdbce67137
SegaGLm <sega.glm@gmail.com>
2024-07-12 12:52:26 +0300

    eng

---
+++ en/app.js
@@ -0,0 +1,15 @@
+VANTA.WAVES({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x576a48,
+    shininess: 33.00,
+    waveHeight: 17.50,
+    waveSpeed: 0.65,
+    zoom: 1.05
+})
